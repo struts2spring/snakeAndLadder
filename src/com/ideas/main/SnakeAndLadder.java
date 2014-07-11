b97a0adfda1068098c7a95d492a1d1ab8d6f35e6
struts2spring <struts2spring@gmail.com>
2014-07-11 15:35:51 +0530

    Update SnakeAndLadder.java

--- src/com/ideas/main/SnakeAndLadder.java
+++ src/com/ideas/main/SnakeAndLadder.java
@@ -5,33 +5,51 @@ import java.util.HashMap;
 import com.ideas.domain.Player;
 import com.ideas.domain.Transporter;
 
+/**
+ * Main class to start with snake and ladder.
+ * 
+ * @author vk82286
+ *
+ */
 public class SnakeAndLadder {
-	Player player1 = null;
-	Player player2 = null;
+	Player[] players = null;
 	HashMap<Integer, Transporter> transporterMap;
 	int numberOfPositionsInBoard = 10 * 12;
+	int numberOfPlayer;
+	int numberOfSnake;
+	int numberOfLadder;
 
 	public static void main(String[] args) {
 
-		int numberOfPlayer = 2;
-		int numberOfSnake = 7;
-		int numberOfLadder = 7;
 		int numberOfPositionsInBoard = 10 * 12;
-		System.out.println("------------------------------------------------------");
-		System.out.println("| A new SnakeAndLadder game of board size 10 * 12 = " + numberOfPositionsInBoard +" |");
-		System.out.println("| Two player are there in this game Player One and Player Two"+" |");
-		System.out.println("| \t 1. snake head and tail are as \n| \t      { 31, 14 }, { 37, 17 }, { 73, 53 }, { 78, 39 }, { 92, 35 }, { 99, 7 }, { 98, 2 } "+" |");
-		System.out.println("| \t 2. ladder start and end are as \n| \t    { 5, 25 }, { 10, 29 }, { 22, 41 }, { 28, 55 }, { 44, 95 }, { 70, 89 }, { 79, 81 } "+" |");
+		System.out.println("---------------------------------------------------------------------------------------------------------");
+		System.out.println("| A new SnakeAndLadder game of board size 10 * 12 = " + numberOfPositionsInBoard + " \t\t\t\t\t\t|");
+		System.out.println("| Two player are there in this game Player One and Player Two" + " \t\t\t\t\t\t|");
+		System.out
+				.println("| \t 1. snake head and tail are as \t\t\t\t\t\t\t\t\t|\n| \t { 31, 14 }, { 37, 17 }, { 73, 53 }, { 78, 39 }, { 92, 35 }, { 99, 7 }, { 98, 2 } "
+						+ " \t\t|");
+		System.out
+				.println("| \t 2. ladder start and end are as \t\t\t\t\t\t\t\t|\n| \t { 5, 25 }, { 10, 29 }, { 22, 41 }, { 28, 55 }, { 44, 95 }, { 70, 89 }, { 79, 81 } "
+						+ " \t\t|");
 		SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
 		// snakeAndLadder.loadGame();
 		// System.out.println(snakeAndLadder.rollDice());
-		System.out.println("| A new game begin."+" |");
-		System.out.println("------------------------------------------------------");
-		snakeAndLadder.beginNewGame();
+		System.out.println("| A new game begin." + " \t\t\t\t\t\t\t\t\t\t\t|");
+		System.out.println("---------------------------------------------------------------------------------------------------------");
+		try {
+			snakeAndLadder.beginNewGame();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 
 	}
 
-	void loadGame() {
+	/**
+	 * Loading of game . Loading snakes, ladder and player.
+	 * 
+	 * @throws Exception
+	 */
+	void loadGame() throws Exception {
 		// Position of snakes for eg. snake at 31 bites and fall to 14
 		// String snakes = "31-14,37-17,73-53,78-39,92-35,99-7";
 
@@ -40,6 +58,11 @@ public class SnakeAndLadder {
 
 		int[][] snakes = { { 31, 14 }, { 37, 17 }, { 73, 53 }, { 78, 39 }, { 92, 35 }, { 99, 7 }, { 98, 2 } };
 		int[][] ladders = { { 5, 25 }, { 10, 29 }, { 22, 41 }, { 28, 55 }, { 44, 95 }, { 70, 89 }, { 79, 81 } };
+		String[] playerNames = { "Player One", "Player Two" };
+		players = new Player[playerNames.length];
+
+		numberOfSnake = snakes.length;
+		numberOfLadder = ladders.length;
 		transporterMap = new HashMap<Integer, Transporter>();
 		for (int[] s : snakes) {
 			Transporter transporter = new Transporter(s[0], s[1], "snake");
@@ -51,11 +74,15 @@ public class SnakeAndLadder {
 			transporterMap.put(ladder[0], transporter);
 		}
 
-//		System.out.println(transporterMap);
+		// System.out.println(transporterMap);
 
-		player1 = new Player(0, "Player One");
-		player2 = new Player(0, "Player Two");
+		int i = 0;
+		for (String name : playerNames) {
+			players[i] = new Player(i, 0, name);
+			i++;
+		}
 
+		System.out.println("");
 		// Position of ladders for eg. Ladder at 5 takes to 25
 	}
 
@@ -65,10 +92,10 @@ public class SnakeAndLadder {
 	// Getting a ladder end top position if reached to a ladder.
 	// rolling the dice. to get a number to move in board.
 
-	/*
+	/**
 	 * A rolling dice will produce a number between 1 to 6.
 	 */
-	public int rollDice() {
+	public int rollDice() throws Exception {
 		int i = 0;
 
 		while (i == 0) {
@@ -78,34 +105,35 @@ public class SnakeAndLadder {
 		return i;
 	}
 
-	public void beginNewGame() {
+	/**
+	 * Game begin from here.
+	 * 
+	 * @throws Exception
+	 */
+	public void beginNewGame() throws Exception {
 		loadGame();
 		Player winner = null;
-		Player player;
+		Player player = null;
 		int i = 0;
 		while (winner == null) {
-			if (i % 2 == 0) {
 
-				player = player1;
-			} else {
-				player = player2;
-			}
+			player = players[i % 2];
 			int diceOutput = rollDice();
-			System.out.println( player.getName() + " from current postion " + player.getPosition()
-					+ " rolled dice and Output is:" + diceOutput);
+			System.out.println(player.getName() + " from current postion " + player.getPosition() + " rolled dice and Output is:"
+					+ diceOutput);
 			int aliasPosition = player.getPosition() + diceOutput;
 
 			// Checking for transporter
-			//System.out.println(transporterMap.get(aliasPosition));
+			// System.out.println(transporterMap.get(aliasPosition));
 			if (transporterMap.get(aliasPosition) != null) {
 				Transporter transporter = transporterMap.get(aliasPosition);
-				if("snake".equals(transporter.getTypeOfTransporter())){
+				if ("snake".equals(transporter.getTypeOfTransporter())) {
 					System.out.println(player.getName() + " got snake bite");
-				}else if("ladder".equals(transporter.getTypeOfTransporter())){
+				} else if ("ladder".equals(transporter.getTypeOfTransporter())) {
 					System.out.println(player.getName() + " got ladder");
 				}
 				player.setPosition(transporter.getEndPoint());
-				
+
 			} else {
 				if (aliasPosition <= numberOfPositionsInBoard) {
 					player.setPosition(aliasPosition);
@@ -115,7 +143,7 @@ public class SnakeAndLadder {
 			// Check player position for winner to be define.
 			if (player.getPosition() == numberOfPositionsInBoard) {
 				winner = player;
-				System.out.println("!!! "+player.getName() +" won !!!");
+				System.out.println("!!! " + player.getName() + " won !!!");
 			}
 			System.out.println("");
 			i++;
